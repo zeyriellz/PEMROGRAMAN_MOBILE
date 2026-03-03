792bc0e713b658a30805020aa95dd023571b9c30
netyst <pineapplekuu@gmail.com>
2026-03-03 23:16:39 +0700

    langkah 1

--- P3/praktikum01.dart
+++ P3/praktikum01.dart
@@ -1,13 +1,14 @@
 //langkah 1
 void main() {
-  String test = "test2";
-  if (test == "test1") {
-    print("Test1");
-  } else if (test == "test2") {
-    print("Test2");
-  } else {
-    print("Something else");
-  }
-
-  if (test == "test2") print("Test2 again");
+String test = "test2";
+if (test == "test1") {
+   print("Test1");
+} else If (test == "test2") {
+   print("Test2");
+} Else {
+   print("Something else");
 }
+
+if (test == "test2") print("Test2 again");
+}
+
