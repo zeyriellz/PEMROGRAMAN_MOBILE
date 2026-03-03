159d5c4ee9630d2b02fc5476aa97bccc5e8dc983
netyst <pineapplekuu@gmail.com>
2026-03-04 00:55:34 +0700

    perbaikan error

--- P3/praktikum03.dart
+++ P3/praktikum03.dart
@@ -2,8 +2,8 @@ void main () {
 for (int index = 10; index < 27; index++) {
   print(index);
 
-If (Index == 21) break;
-Else If (index > 1 || index < 7) continue;
+if (index == 21) break;
+else if (index > 1 && index < 7) continue;
 print(index);
 }
 }
