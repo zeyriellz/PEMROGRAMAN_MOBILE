7a8a5f4ead7de2cf4304111e3102505fd218e482
netyst <pineapplekuu@gmail.com>
2026-03-03 23:37:52 +0700

    modifikasi

--- P3/praktikum01.dart
+++ P3/praktikum01.dart
@@ -2,11 +2,11 @@
 void main() {
 String test = "test2";
 if (test == "test1") {
-   print("Test1");
+  print("Test1");
 } else If (test == "test2") {
-   print("Test2");
+  print("Test2");
 } Else {
-   print("Something else");
+  print("Something else");
 }
 
 if (test == "test2") print("Test2 again");
