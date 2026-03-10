5202f505ef676246b7c182a04d295fe83294d97a
netyst <pineapplekuu@gmail.com>
2026-03-10 23:28:31 +0700

    langkah 3

--- P4/praktikum02.dart
+++ P4/praktikum02.dart
@@ -1,4 +1,12 @@
 void main () {
   var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
   print(halogens);
+
+  var names1 = <String>{};
+  Set<String> names2 = {}; 
+  var names3 = {}; 
+
+  print(names1);
+  print(names2);
+  print(names3);
 }
